e5f8ddfe040d0b425be375a6a3f22bc010449b8f
Wojciech Kwolek <me@irth.pl>
2017-02-25 20:22:42 +0100

    fixed a bug where updating a task deleted it

--- src/actions/actionTypes.jsx
+++ src/actions/actionTypes.jsx
@@ -1,6 +1,6 @@
 export const ADD_TASK = 'ADD_TASK'
 export const REMOVE_TASK = 'REMOVE_TASK'
-export const UPDATE_TASK = 'REMOVE_TASK'
+export const UPDATE_TASK = 'UPDATE_TASK'
 
 export const START_TIMER = 'START_TIMER'
 export const PAUSE_TIMER = 'PAUSE_TIMER'
